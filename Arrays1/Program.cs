d87ae1f768362e858bd59f6ea59eb3fdf7c0b56a
Nikita <nikosh1947@gmail.com>
2019-10-12 21:51:45 +0300

    Arrays

---
+++ Arrays1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] m = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива: ");
+                m[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Sum(ref n, m);
+            Max(ref n, m);
+            MaxAbs(ref n, m);
+            SumNumbers(ref n, m);
+            Int(ref n, m);
+        }
+
+        static void Sum(ref int n, params double[] m)
+        {
+            double s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (m[i] < 0)
+                {
+                    s = s + m[i];
+                }
+            }
+            Console.WriteLine($"Сумма отрицательных элементов массива: {s}");
+        }
+
+        static void Max(ref int n, params double[] m)
+        {
+            double max = -2480866868, nm = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (m[i] > max)
+                {
+                    max = m[i];
+                    nm = i + 1;
+                }
+            }
+            Console.WriteLine($"Максимальный элемент массива: {max}");
+            MaxNumber(ref nm);
+        }
+
+        static void MaxNumber(ref double nm)
+        {
+            Console.WriteLine($"Номер максимального элемента массива: {nm}");
+        }
+
+        static void MaxAbs(ref int n, params double[] m)
+        {
+            double maxa = -2480866868;
+            double q = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (Math.Abs(m[i]) > maxa)
+                {
+                    maxa = Math.Abs(m[i]);
+                    q = m[i];
+                }
+            }
+            Console.WriteLine($"Максимальный за модулем элемент массива: {q}");
+        }
+
+        static void SumNumbers(ref int n, params double[] m)
+        {
+            int p = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(m[i] > 0)
+                {
+                    p = p + i + 1;
+                }
+            }
+            Console.WriteLine($"Сумма номеров положительных элементов массива: {p}");
+        }
+
+        static void Int(ref int n, params double[] m)
+        {
+            int q = 0, s = 0;
+            for(int i = 0; i < n; i++)
+            {
+                q = (int)m[i];
+                if(m[i] - q == 0)
+                {
+                    s++;
+                }
+            }
+            Console.WriteLine($"Количество целых чисел в массиве: {s}");
+        }
+    }
+}
---
+++ Arrays2/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] m = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива: ");
+                m[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            MyltMin(ref n, m);
+            SumMinusPlus(ref n, m);
+            SumNull(ref n, m);
+            DobMaxAbsMinAbs(ref n, m);
+        }
+
+        static void MyltMin(ref int n, params double[] m)
+        {
+            double min = 2480866868, s = 1;
+            int mini = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (min > m[i])
+                {
+                    min = m[i];
+                    mini = i;
+                }
+            }
+            if (mini + 1 == m.Length)
+            {
+                s = 0;
+            }
+            else
+            {
+                for (int i = mini + 1; i < n; i++)
+                {
+                    s = s * m[i];
+                }
+            }
+            Console.WriteLine($"Произведение всех чисел после минимального: {s}");
+        }
+
+        static void SumMinusPlus(ref int n, params double[] m)
+        {
+            int pv = 0, pd = -1, dd = -1;
+            double s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (m[i] < 0)
+                {
+                    pv = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (m[i] > 0 && pd != 0)
+                {
+                    dd = i;
+                    break;
+                }
+                else if (m[i] > 0 && pd == 0)
+                {
+                    pd = i;
+                }
+            }
+            if (pv < dd && pv != -1 && dd != -1)
+            {
+                for (int i = pv; i < dd; i++)
+                {
+                    s = s + m[i];
+                }
+                Console.WriteLine($"Сумма элементов от первого отрицательного до второго положительного: {s}");
+            }
+            else
+            {
+                Console.WriteLine("Между первым отрицательным и вторым положительным элементов массива нет элементов");
+            }
+        }
+
+        static void SumNull(ref int n, params double[] m)
+        {
+            int firstNull = -1, lastNull = -1;
+            double s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (m[i] == 0)
+                {
+                    firstNull = i;
+                    break;
+                }
+            }
+            for (int i = m.Length - 1; i >= 0; i--)
+            {
+                if (m[i] == 0)
+                {
+                    lastNull = i;
+                    break;
+                }
+            }
+            if (firstNull == -1 || lastNull == -1)
+            {
+                Console.WriteLine("В массиве меньше двух нулевых элементов");
+            }
+            else
+            {
+                for (int i = firstNull; i < lastNull; i++)
+                {
+                    s = s + m[i];
+                }
+                Console.WriteLine($"Сумма элементов от первого нулевого до последнего нулевого элементов: {s}");
+            }
+        }
+
+        static void DobMaxAbsMinAbs(ref int n, params double[] m)
+        {
+            double maxAbs = -2480866868, minAbs = 2480866868, s = 1;
+            int maxi = 0, mini = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (maxAbs < Math.Abs(m[i]))
+                {
+                    maxAbs = Math.Abs(m[i]);
+                    maxi = i;
+                }
+                if (minAbs > Math.Abs(m[i]))
+                {
+                    minAbs = Math.Abs(m[i]);
+                    mini = i;
+                }
+            }
+            if (maxi > mini)
+            {
+                Console.WriteLine("Между максимальным и минимальным элементом нет элементов");
+            }
+            else
+            {
+                for (int i = maxi + 1; i < mini - 1; i++)
+                {
+                    s = s * m[i];
+                }
+                Console.WriteLine($"Произвидение элементов между максимальным и минимальным элементами: {s}");
+            }
+        }
+    }
+}
